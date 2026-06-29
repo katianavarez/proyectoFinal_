d7be015c5a3a8dd275eacd9b38ce67db0a619c4d
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2026-06-29 00:06:58 -0700

    Update AppNavigation.kt

--- app/src/main/java/navarez/katia/proyectofinal/navigation/AppNavigation.kt
+++ app/src/main/java/navarez/katia/proyectofinal/navigation/AppNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import navarez.katia.proyectofinal.ui.screens.ListaLibrosScreen
 import navarez.katia.proyectofinal.ui.screens.LoginScreen
 import navarez.katia.proyectofinal.ui.screens.RegistroScreen
 
@@ -17,6 +18,9 @@ fun AppNavigation() {
         }
         composable(Screen.Registro.route) {
             RegistroScreen(navController)
+        }
+        composable(Screen.ListaLibros.route) {
+            ListaLibrosScreen(navController)
         }
     }
 }
